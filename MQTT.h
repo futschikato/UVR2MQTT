0620d02aed8c7ec97429412f26e4491747d22f42
futschikato <46090941+futschikato@users.noreply.github.com>
2019-01-29 20:12:36 +0100

    Update MQTT.h

--- MQTT.h
+++ MQTT.h
@@ -37,7 +37,7 @@ void mqtt_daten_senden() {
   // Sensordaten über mqtt versenden
   for (int sv = 1; sv <= 16; sv++) {
     if (mqtt_client.connected()) {
-      topic = "/openHAB/UVR1611/Sensor" + String(sv);
+      topic = "UVR2MQTT/Sensor" + String(sv);
       Serial.println("MQTT: " + topic + ": "+ String(SensorValue[sv]));
       mqtt_client.publish(topic.c_str(), SensorValue[sv].c_str());
     } 
@@ -47,12 +47,12 @@ void mqtt_daten_senden() {
   }
   if (mqtt_client.connected()) {
   //Wärmemengenzähler über mqtt versenden
-    mqtt_client.publish("/openHAB/UVR1611/WMZ1currentPower", WMZ1[1].c_str());
-    mqtt_client.publish("/openHAB/UVR1611/WMZ1kwh", WMZ1[2].c_str());
-    mqtt_client.publish("/openHAB/UVR1611/WMZ1mwh", WMZ1[3].c_str());
-    mqtt_client.publish("/openHAB/UVR1611/WMZ2currentPower", WMZ2[1].c_str());
-    mqtt_client.publish("/openHAB/UVR1611/WMZ2kwh", WMZ2[2].c_str());
-    mqtt_client.publish("/openHAB/UVR1611/WMZ2mwh", WMZ2[3].c_str());
+    mqtt_client.publish("UVR2MQTT/WMZ1currentPower", WMZ1[1].c_str());
+    mqtt_client.publish("UVR2MQTT/WMZ1kwh", WMZ1[2].c_str());
+    mqtt_client.publish("UVR2MQTT/WMZ1mwh", WMZ1[3].c_str());
+    mqtt_client.publish("UVR2MQTT/WMZ2currentPower", WMZ2[1].c_str());
+    mqtt_client.publish("UVR2MQTT/WMZ2kwh", WMZ2[2].c_str());
+    mqtt_client.publish("UVR2MQTT/WMZ2mwh", WMZ2[3].c_str());
   } 
   else {
       mqtt_connect();
@@ -61,7 +61,7 @@ void mqtt_daten_senden() {
   // Zustände der Ausgänge übertragen
   for (int sv = 1; sv <= 13; sv++) {
     if (mqtt_client.connected()) {
-      topic = "/openHAB/UVR1611/Ausgang" + String(sv);
+      topic = "UVR2MQTT/Ausgang" + String(sv);
       Serial.println("MQTT: " + topic + ": "+ Ausgang[sv]);
       mqtt_client.publish(topic.c_str(), Ausgang[sv]? "1" : "0");
     } 
@@ -73,7 +73,7 @@ void mqtt_daten_senden() {
   // Drehzahl Ausgänge übertragen
   if (mqtt_client.connected()) {
     for (int sv = 1; sv <= 4; sv++) {
-      topic = "/openHAB/UVR1611/Drehzahl" + String(sv);
+      topic = "UVR2MQTT/Drehzahl" + String(sv);
       Serial.println("MQTT: " + topic + ": "+ Drehzahl[sv]);
       mqtt_client.publish(topic.c_str(), Drehzahl[sv].c_str());
     } 
@@ -84,7 +84,7 @@ void mqtt_daten_senden() {
   // WLAN RSSI
   if (mqtt_client.connected()) {
     rssi = WiFi.RSSI();
-    topic = "/openHAB/UVR1611/RSSI";
+    topic = "UVR2MQTT/RSSI";
     Serial.println("MQTT: " + topic + ": "+ rssi);
     mqtt_client.publish(topic.c_str(), rssi.c_str());
   } else {
